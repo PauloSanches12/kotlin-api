ed8ac87ca6d6ae964bf72e7ca135d26109a2f175
Paulo <paulo_sanches2012@hotmail.com>
2021-05-13 14:48:41 -0300

    Created AccountControllerTest, add test find all

---
+++ src/test/kotlin/com/paulo/bank/AccountControllerTest.kt
@@ -0,0 +1,31 @@
+package com.paulo.bank
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AccountControllerTest {
+
+    @Autowired lateinit var mockMvc: MockMvc
+    @Autowired lateinit var accountRepository: AccountRepository
+
+    @Test
+    fun `test find all`() {
+        accountRepository.save(Account(name = "Test Find All", document = "123", phone = "12345"))
+        mockMvc.perform(MockMvcRequestBuilders.get("/accounts"))
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andExpect(MockMvcResultMatchers.jsonPath("\$").isArray)
+            .andExpect(MockMvcResultMatchers.jsonPath("\$[0].id").isNumber)
+            .andExpect(MockMvcResultMatchers.jsonPath("\$[0].name").isString)
+            .andExpect(MockMvcResultMatchers.jsonPath("\$[0].document").isString)
+            .andExpect(MockMvcResultMatchers.jsonPath("\$[0].phone").isString)
+            .andDo(MockMvcResultHandlers.print())
+    }
+}
