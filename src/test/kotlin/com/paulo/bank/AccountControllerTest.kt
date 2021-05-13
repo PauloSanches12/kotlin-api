5004ab9dd3e32fbe1c21186032c8fc2da618693f
Paulo <paulo_sanches2012@hotmail.com>
2021-05-13 14:58:39 -0300

    Created test find by id

--- src/test/kotlin/com/paulo/bank/AccountControllerTest.kt
+++ src/test/kotlin/com/paulo/bank/AccountControllerTest.kt
@@ -28,4 +28,16 @@ class AccountControllerTest {
             .andExpect(MockMvcResultMatchers.jsonPath("\$[0].phone").isString)
             .andDo(MockMvcResultHandlers.print())
     }
+
+    @Test
+    fun `test find by id`() {
+        val account = accountRepository.save(Account(name = "Test Find All", document = "123", phone = "12345"))
+        mockMvc.perform(MockMvcRequestBuilders.get("/accounts/${account.id}"))
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andExpect(MockMvcResultMatchers.jsonPath("\$.id").value(account.id))
+            .andExpect(MockMvcResultMatchers.jsonPath("\$.name").value(account.name))
+            .andExpect(MockMvcResultMatchers.jsonPath("\$.document").value(account.document))
+            .andExpect(MockMvcResultMatchers.jsonPath("\$.phone").value(account.phone))
+            .andDo(MockMvcResultHandlers.print())
+    }
 }
